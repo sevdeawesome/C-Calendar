154e4437e2b3ea88507fe98e4c1c8a5372851eb5
Carl <carlche@carlche.com>
2020-12-08 15:39:01 -0800

    adding FileIO tests

---
+++ tests/FileIOTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "FileIO.h"
+
+using namespace std;
+
+int main()
+{
+	FileIO::GetInstance()->readFromFile("readFile.txt");
+	FileIO::GetInstance()->showAllDatas();
+	FileIO::GetInstance()->addData(6);
+	FileIO::GetInstance()->addData(7);
+	cout<<FileIO::GetInstance()->getData(2)<<endl;
+	FileIO::GetInstance()->writeToFile("writeFile.txt");
+	return 0;
+}
