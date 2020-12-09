eb574dfe501d76445ceb54f4918da512fb848563
Severin Field <sevdeawesome@gmail.com>
2020-12-09 02:36:12 -0800

    Update test.cpp
    blank testing file

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 //#include "time_tests.h"
-#include "event_tests.h"
+//#include "event_tests.h"
+//include other tests here
 #include "../header/Event.hpp"
 
 int main(int argc, char **argv) {
