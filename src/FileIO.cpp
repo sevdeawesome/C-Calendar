a6cff9c628bc613a6fb6c2d118ba7c7cb58b6a67
Carl <carlche@carlche.com>
2020-12-08 21:08:12 -0800

    adding string lib

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 #include "FileIO.h"
 
 using namespace std;
--- tests/FileIOTest.cpp
+++ tests/FileIOTest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "FileIO.h"
 
 using namespace std;
