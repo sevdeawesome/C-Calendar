a74e8396d086635e2d854d47a2bc2546e2574572
Carl <carlche@carlche.com>
2020-12-10 01:42:55 -0800

    adding FileIOTest.hpp

---
+++ tests/FileIOTest.hpp
@@ -0,0 +1,13 @@
+#ifndef __FILEIOTEST_HPP_
+#define __FILEIOTEST_HPP_
+
+#include "../header/FileIO.hpp"
+
+TEST(FileIOTests, ReadFromFileTest)
+{
+	FileIO::GetInstance()->readFromFile("winter2020.txt");
+	EXPECT_EQ(FileIO::GetInstance()->showAllData(), "1\n2\n3\n4\n5\n");
+} 
+
+
+#endif
