a34780b659b572e4bf13de9a01b699a30bc6028f
Carl <carlche@carlche.com>
2020-12-10 14:34:24 -0800

    finishing up FileIO tests

--- tests/FileIOTest.hpp
+++ tests/FileIOTest.hpp
@@ -3,11 +3,43 @@
 
 #include "../header/FileIO.hpp"
 
+
 TEST(FileIOTests, ReadFromFileAndShowDataTest)
 {
-	FileIO::GetInstance()->readFromFile("winter2020.txt");
+	FileIO::GetInstance()->readFromFile("readFile.txt");
 	EXPECT_EQ(FileIO::GetInstance()->showAllData(), "1\n2\n3\n4\n5\n");
-} 
+	ASSERT_NE(FileIO::GetInstance()->showAllData(), "21\n2\n3\n4\n5\n");
+	ASSERT_TRUE(FileIO::GetInstance()->showAllData() == "1\n2\n3\n4\n5\n");
+	ASSERT_FALSE(FileIO::GetInstance()->showAllData() == "21\n2\n3\n4\n5\n");
+}
+TEST(FileIOTests, AddDataTest)
+{
+	FileIO::GetInstance()->readFromFile("readFile.txt");
+	FileIO::GetInstance()->addData("6");
+	EXPECT_EQ(FileIO::GetInstance()->showAllData(), "1\n2\n3\n4\n5\n6\n");
+	ASSERT_NE(FileIO::GetInstance()->showAllData(), "21\n2\n3\n4\n5\n6\n");
+	ASSERT_TRUE(FileIO::GetInstance()->showAllData() == "1\n2\n3\n4\n5\n6\n");
+	ASSERT_FALSE(FileIO::GetInstance()->showAllData() == "21\n2\n3\n4\n5\n6\n");
+}
+TEST(FileIOTests, GetDataTest)
+{
+	FileIO::GetInstance()->readFromFile("readFile.txt");
+	EXPECT_EQ(FileIO::GetInstance()->getData(0), "1");
+	ASSERT_NE(FileIO::GetInstance()->getData(0), "21");
+	ASSERT_TRUE(FileIO::GetInstance()->getData(0) == "1");
+	ASSERT_FALSE(FileIO::GetInstance()->getData(0) == "21");
+}
+TEST(FileIOTests, WriteToFileAndShowDataTest)
+{
+	FileIO::GetInstance()->readFromFile("readFile.txt");
+	FileIO::GetInstance()->addData("6");
+	FileIO::GetInstance()->writeToFile("writeFile.txt");
+	FileIO::GetInstance()->readFromFile("readFile.txt");
+	EXPECT_EQ(FileIO::GetInstance()->showAllData(), "1\n2\n3\n4\n5\n6\n");
+	ASSERT_NE(FileIO::GetInstance()->showAllData(), "21\n2\n3\n4\n5\n6\n");
+	ASSERT_TRUE(FileIO::GetInstance()->showAllData() == "1\n2\n3\n4\n5\n6\n");
+	ASSERT_FALSE(FileIO::GetInstance()->showAllData() == "21\n2\n3\n4\n5\n6\n");
+}
 
 
 #endif
